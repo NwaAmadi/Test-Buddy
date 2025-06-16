78eb245303d01945c0ea911f5986f80691b3ba73
Gospel <chibuisiukegbu@gmail.com>
2025-06-16 11:16:21 +0100

    chore: wroking on the results page

--- server/server.ts
+++ server/server.ts
@@ -200,6 +200,7 @@ app.post('/api/otp-verify', async (req: Request, res: Response): Promise<any> =>
       accessToken,
       refreshToken,
       user: {
+        id: user.id,
         email: user.email,
         first_name: user.first_name,
         last_name: user.last_name,
