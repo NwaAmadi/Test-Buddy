194c25245540e41aae919591c2728bda9e81e197
Gospel <chibuisiukegbu@gmail.com>
2025-06-17 13:42:59 +0100

    Chore: working on the admin section

--- server/server.ts
+++ server/server.ts
@@ -407,7 +407,7 @@ app.use("/api/result", resultRoute);
 app.use("/api/exam", submitExamRoute);
 app.use("/student/results", allResultsRouter);
 
-app.use("/api/admin/exams", examsRoutes)
+app.use("/api/admin/exam", examsRoutes)
 app.use("/api/admin/questions", questionsRoutes)
 app.use("/api/admin/results", resultsRoutes)
 
